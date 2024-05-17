2a0d7bc8eed194d25ab6b861009a7b9640dcc104
jeontaeho <jth121091@gmail.com>
2024-05-17 14:15:15 +0900

    feat:정기예금표출력기능추가

--- front/src/stores/finance.js
+++ front/src/stores/finance.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 
 export const useFinanceStore = defineStore('finance', () => {
   const API_URL = 'http://127.0.0.1:8000'
+  const finances = ref([])
   const token = ref(null)
   
   const getDeposits = function () {
@@ -13,12 +14,13 @@ export const useFinanceStore = defineStore('finance', () => {
       url: `${API_URL}/deposit/products/`
     }).then(response => {
       // console.log(response)
-      console.log(response)
+      finances.value = response.data
+      console.log(response.data)
     }).catch(error => {
       console.log(error)
     })
   }
 
 
-  return { API_URL, getDeposits }
+  return { API_URL, getDeposits, finances }
 })
--- front/src/views/CompareView.vue
+++ front/src/views/CompareView.vue
@@ -11,70 +11,76 @@
     <title>예/적금 금리비교</title>
   </head>
 
-  <div class="container mt-5">
-    <div class="dropdown">
-      <button
-        class="btn btn-primary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        {{ selectedText }}
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li>
-          <a class="dropdown-item" href="#" @click="setDropdownText('6개월')">6개월</a>
-        </li>
-        <li>
-          <a class="dropdown-item" href="#" @click="setDropdownText('12개월')">12개월</a>
-        </li>
-        <li>
-          <a class="dropdown-item" href="#" @click="setDropdownText('24개월')">24개월</a>
-        </li>
-        <li>
-          <a class="dropdown-item" href="#" @click="setDropdownText('36개월')">36개월</a>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
   <body>
     <div class="container-fluid">
       <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <span data-feather="home"></span>
-                  Dashboard <span class="sr-only">(current)</span>
-                </a>
-              </li>
+              <div class="container mt-5">
+                <div class="dropdown">
+                  정렬기준
+                  <button
+                    class="btn btn-primary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {{ selectedText }}
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('1개월')">1개월</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('3개월')">3개월</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('6개월')">6개월</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('12개월')">12개월</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('24개월')">24개월</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#" @click="setDropdownText('36개월')">36개월</a>
+                    </li>
+                    
+                    </ul>
+                  </div>
+              </div>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="file"></span>
                   Orders
                 </a>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="shopping-cart"></span>
                   Products
                 </a>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="users"></span>
                   Customers
                 </a>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="bar-chart-2"></span>
                   Reports
                 </a>
               </li>
+              
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="layers"></span>
@@ -119,146 +125,44 @@
         </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-              </div>
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div>
-          </div>
-
-          <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
-
-          <h2>Section title</h2>
+          <h2>정기예금</h2>
           <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>금융회사명</th>
+                  <th>상품명</th>
+                  <th>1개월</th>
+                  <th>3개월</th>
+                  <th>6개월</th>
+                  <th>12개월</th>
+                  <th>24개월</th>
+                  <th>36개월</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
+                <tr v-for="finance in store.finances" :key="finance.base_product_id">
+                  <td>{{ finance.kor_co_nm }}</td>
+                  <td>{{ finance.fin_prdt_nm }}</td>
+                  <!-- 1개월 -->
+                  <td v-if="finance.options[0]">{{ finance.options[0]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  <!-- 3개월 -->
+                  <td v-if="finance.options[1]">{{ finance.options[1]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  <!-- 6개월 -->
+                  <td v-if="finance.options[2]">{{ finance.options[2]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  <!-- 12개월 -->
+                  <td v-if="finance.options[3]">{{ finance.options[3]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  <!-- 24개월 -->
+                  <td v-if="finance.options[4]">{{ finance.options[4]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  <!-- 36개월 -->
+                  <td v-if="finance.options[5]">{{ finance.options[5]['intr_rate2']}}</td>
+                  <td v-else>-</td>
+                  
                 </tr>
               </tbody>
             </table>
@@ -273,7 +177,7 @@
 import { useFinanceStore } from '@/stores/finance'
 import { ref } from 'vue'
 import axios from 'axios'
-const store = useFinanceStore()
+const store = useFinanceStore() 
 store.getDeposits()
 
 // 이제 드랍다운 버튼에 따라 선택된 값을 django에 보내기
@@ -294,7 +198,7 @@ const sendPeriod = function () {
 
 
 // 기본 예/적금 예치기간은 default가 전체기간
-const selectedText = ref('전체기간')
+const selectedText = ref('12개월')
 // 예/적금 예치기간 버튼을 위한 함수
 const setDropdownText = function (text) {
   selectedText.value = text
