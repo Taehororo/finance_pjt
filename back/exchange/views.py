978f86acdb87a6c781ad862ceeaabd3463d93aa6
kaxadlec <kaxadlec@gmail.com>
2024-05-22 15:35:06 +0900

    feat: 환율 그래프 위한 데이터 보내기

--- back/exchange/views.py
+++ back/exchange/views.py
@@ -12,13 +12,11 @@ from datetime import datetime, timedelta
 # 글로벌 변수로 환율 데이터를 저장
 EXCHANGE_DATA = None
 EXCHANGE_DATE = None
-
+# API 키와 URL을 설정
+API_KEY = settings.EXCHANGE_API_KEY
+URL = f'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
 
 def fetch_exchange_rates(search_date):
-    # API 키와 URL을 설정
-    API_KEY = settings.EXCHANGE_API_KEY
-    URL = f'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
-
     # 날짜를 'YYYYMMDD' 형식으로 변환
     search_date_str = search_date.strftime('%Y%m%d')
     params = {
@@ -53,6 +51,46 @@ def get_exchange_rates(request):
     return Response({'message': 'Exchange rates fetched successfully'}, status=status.HTTP_200_OK)
 
 
+# 두개 국가의 일주일 간 dates과 rates 배열을 반환 하는 함수
+def fetch_dates_and_rates_for_period(first_country, second_country):
+    # 오늘 날짜 설정
+    today = datetime.today()
+    dates = [(today - timedelta(days=i)).strftime('%Y%m%d') for i in range(10)] 
+
+    # 데이터를 저장할 리스트 초기화
+    first_country_data = []
+    second_country_data = []
+
+    for date in dates:
+        if len(first_country_data) == 7 and len(second_country_data) == 7:
+            break
+
+        url = f'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={API_KEY}&searchdate={date}&data=AP01'
+        response = requests.get(url)
+        data = response.json()
+        if not data:
+            continue
+        
+        # USD 데이터 추출
+        for item in data:
+            if item['cur_nm'] == first_country:
+                rate = float(item['kftc_deal_bas_r'].replace(',', ''))
+                first_country_data.append((date, rate))
+            
+            elif item['cur_nm'] == second_country:
+                rate = float(item['kftc_deal_bas_r'].replace(',', ''))
+                second_country_data.append((date, rate))
+
+        # 날짜 기준으로 정렬
+        first_country_data.sort(key=lambda x: x[0])
+        second_country_data.sort(key=lambda x: x[0])
+
+        first_country_dates, first_country_rates = zip(*first_country_data)
+        second_country_dates, second_country_rates = zip(*second_country_data)
+
+    return first_country_dates, first_country_rates, second_country_dates, second_country_rates
+
+
 @api_view(['POST'])
 def calculate_exchange(request):
     global EXCHANGE_DATA, EXCHANGE_DATE
@@ -63,6 +101,10 @@ def calculate_exchange(request):
             output_country = data.get('outputcountry')
             amount = float(data.get('money'))
             
+            # 특정국가의 날짜 배열과 환율 배열 
+            first_country_dates, first_country_rates, second_country_dates, second_country_rates = fetch_dates_and_rates_for_period(input_country, output_country)
+
+            # 환율계산파트
             if EXCHANGE_DATA is None:
                 return Response({'error': 'Exchange rates not available'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
@@ -85,7 +127,8 @@ def calculate_exchange(request):
                 result = (amount / output_rate_value) * input_rate_value     # 환율 계산
                 result = round(result, 2)     # 결과를 소수점 아래 둘째 자리까지 반올림
                 # 계산된 환율, 날짜 보내기
-                return Response({'result': result, 'exchange_date': EXCHANGE_DATE}, status=status.HTTP_200_OK) 
+                return Response({'result': result, 'exchange_date': EXCHANGE_DATE, 'input_country': {'dates': first_country_dates, 'rates': first_country_rates}, 
+                                'output_country': {'dates': second_country_dates, 'rates': second_country_rates}}, status=status.HTTP_200_OK) 
             else:
                 return Response({'error': 'Invalid currency code'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
